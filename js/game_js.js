284d2ea189088e37b5d95262db197727756c0710
MBaltais <markussbaltais@gmail.com>
2021-05-07 12:30:38 +0300

    update 26/02/2021

--- js/game_js.js
+++ js/game_js.js
@@ -8,7 +8,6 @@
 var icons = ["fa fa-dropbox card", "fa fa-amazon card", "fa fa-bitcoin card", "fa fa-apple card", "fa fa-facebook card", "fa fa-spotify card", "fa fa-twitter card", "fa fa-whatsapp card", "fa fa-behance card", "fa fa-slack card", "fa fa-tripadvisor card", "fa fa-pinterest card", "fa fa-tumblr card", "fa fa-skype card", "fa fa-google card", "fa fa-github card"];
 var brands = ["Dropbox", "Amazon", "Bitcoin", "Apple", "Facebook", "Spotify", "Twitter", "Whatsapp", "Behance", "Slack", "Tripadvisor", "Pinterest", "Tumblr", "Skype", "Google", "Github"];
 var cards = [];
-var temp = [];
 var openedCards = [];
 var dict = {"Dropbox": 0, "Amazon": 1, "Bitcoin": 2, "Apple": 3, "Facebook": 4, "Spotify": 5, "Twitter": 6, "Whatsapp": 7, "Behance": 8, "Slack": 9, "Tripadvisor": 10, "Pinterest": 11, "Tumblr": 12, "Skype": 13, "Google": 14, "Github": 15};
 var dict2 = {"fa fa-dropbox card": 0, "fa fa-amazon card": 1, "fa fa-bitcoin card": 2, "fa fa-apple card": 3, "fa fa-facebook card": 4, "fa fa-spotify card": 5, "fa fa-twitter card": 6, "fa fa-whatsapp card": 7, "fa fa-behance card": 8, "fa fa-slack card": 9, "fa fa-tripadvisor card": 10, "fa fa-pinterest card": 11, "fa fa-tumblr card": 12, "fa fa-skype card": 13, "fa fa-google card": 14, "fa fa-github card": 15};
@@ -22,6 +21,8 @@ var timer = document.querySelector(".timer");
 var interval;
 timer.innerHTML = "0 min 0 sek";
 
+
+// sākotnējā sajaukšana, lai vislaik nebūtu vienas un tās pašas kārtis
 function initialShuffle() {
 
     for (let t = 0; t < icons.length; t++) {
@@ -32,10 +33,11 @@ function initialShuffle() {
         brands[rand] = brands[t];
         icons[t] = tmp;
         brands[t] = tmp2;
-
+        console.log("tmp: " + tmp + " tmp2: " + tmp2);
     }
 }
 
+// iegūst ievadīto vērtību - kāršu pāru skaitu
 function inputValue() {
     var value = document.getElementById("numberOfCards").value;
     var a = icons.slice(0, value);
@@ -44,18 +46,19 @@ function inputValue() {
     brands = b;
 }
 
-
+// funkcija uz kārts klikšķa --> parāda kārti
 var displayCard = function () {
     this.classList.toggle("open");
 };
 
-
+// sajauc kārtis
 function shuffling() {
 
     for (let i = 0; i < icons.length; i++) {
         cards.push(icons[i]);
         cards.push(brands[i]);
     }
+    
     for (let i = 0; i < cards.length; i++) {
         var rand = Math.floor(Math.random() * cards.length);
         var tmp = cards[rand];
@@ -74,93 +77,66 @@ function cardCheck() {
         var span = document.createElement("SPAN");
 
 
-
+        // pārbauda vai ir teksts (tas sākas ar lielo burtu)
+        // teksta value atrod iekš dictionary pēc key innerHTML
         if (char >= "A" && char <= "Z") {
             span.innerHTML = cards[m];
             span.setAttribute("class", "card");
             span.setAttribute("id", "a" + dict[span.innerHTML]);
             span.addEventListener("click", displayCard);
-            span.addEventListener("click", function () {
-                openedCards.push(this.getAttribute("id"));
-                var length = openedCards.length;
-                if (length === 2) {
-                    if (openedCards[0].slice(1) === openedCards[1].slice(1)) {
-
-                        document.getElementById(openedCards[0]).classList.add("match", "disabled");
-                        document.getElementById(openedCards[1]).classList.add("match", "disabled");
-                        document.getElementById(openedCards[0]).classList.remove("show", "open");
-                        document.getElementById(openedCards[1]).classList.remove("show", "open");
-                        value--;
-
-                        if (value === 0) {
-                            congratulations();
-                        } else {
-                            openedCards.pop();
-                            openedCards.pop();
-                        }
-
-                    } else {
-                        document.getElementById(openedCards[0]).classList.add("unmatched");
-                        document.getElementById(openedCards[1]).classList.add("unmatched");
-
-                        setTimeout(function () {
-                            document.getElementById(openedCards[0]).classList.remove("open", "unmatched");
-                            document.getElementById(openedCards[1]).classList.remove("open", "unmatched");
-                            openedCards.pop();
-                            openedCards.pop();;
-                        }, 200);
-                    }
-                }
-            });
             document.getElementById("cardsID").appendChild(span);
 
-
-
-
+           
+        // ja nav teksts, tad ikona
+        // ikonas value atrod iekš dictionary pēc key class
         } else {
             span.setAttribute("class", cards[m]);
             console.log(dict[span.getAttribute("class")]);
             span.setAttribute("id", "b" + dict2[span.getAttribute("class")]);
             span.addEventListener("click", displayCard);
-            span.addEventListener("click", function () {
-
-                openedCards.push(this.getAttribute("id"));
-                var length = openedCards.length;
-                if (length === 2) {
-                    if (openedCards[0].slice(1) === openedCards[1].slice(1)) {
-
-                        document.getElementById(openedCards[0]).classList.add("match", "disabled");
-                        document.getElementById(openedCards[1]).classList.add("match", "disabled");
-                        document.getElementById(openedCards[0]).classList.remove("show", "open");
-                        document.getElementById(openedCards[1]).classList.remove("show", "open");
-                        value--;
-                        if (value === 0) {
-                            congratulations();
-                        } else {
-                            openedCards.pop();
-                            openedCards.pop();;
-                        }
-
-
-                    } else {
-
-                        document.getElementById(openedCards[0]).classList.add("unmatched");
-                        document.getElementById(openedCards[1]).classList.add("unmatched");
-
-                        setTimeout(function () {
-                            document.getElementById(openedCards[0]).classList.remove("open", "unmatched");
-                            document.getElementById(openedCards[1]).classList.remove("open", "unmatched");
-                            openedCards.pop();
-                            openedCards.pop();;
-                        }, 200);
-                    }
-                }
-            });
             document.getElementById("cardsID").appendChild(span);
         }
+        
+        // reakcija uz kārts klikšķa
+        span.addEventListener("click", function() {
+            openedCards.push(this.getAttribute("id"));
+            var length = openedCards.length;
+            if (length === 2) {
+                
+                //pārbauda vai id numuri ir vienādi (piem. a12, b12)
+                if (openedCards[0].slice(1) === openedCards[1].slice(1)) {
+
+                    //classListes ir definētas iekš css
+                    document.getElementById(openedCards[0]).classList.add("match", "disabled");
+                    document.getElementById(openedCards[1]).classList.add("match", "disabled");
+                    document.getElementById(openedCards[0]).classList.remove("show", "open");
+                    document.getElementById(openedCards[1]).classList.remove("show", "open");
+                    value--;
+                    
+                    // ja value ir 0, pabeidz spēli, ja nav --> turpina
+                    if (value === 0) {
+                        congratulations();
+                    } else {
+                        openedCards = [];
+                    }
+
+                } else {
+                    document.getElementById(openedCards[0]).classList.add("unmatched");
+                    document.getElementById(openedCards[1]).classList.add("unmatched");
+
+                    setTimeout(function () {
+                        document.getElementById(openedCards[0]).classList.remove("open", "unmatched");
+                        document.getElementById(openedCards[1]).classList.remove("open", "unmatched");
+                        openedCards.pop();
+                        openedCards.pop();;
+                    }, 600);
+                }
+            }
+        });
     }
 }
 
+//popup paziņojums
 function congratulations() {
     clearInterval(interval);
     finalTime = timer.innerHTML;
@@ -172,6 +148,7 @@ function congratulations() {
 }
 
 
+// laika atskaite
 function startTimer() {
     interval = setInterval(function () {
         timer.innerHTML = min + " min " + sec + " sek";
@@ -188,13 +165,34 @@ function startTimer() {
 }
 
 
+// spēles kods
 function startGame() {
-
-    document.getElementById("cardsID").innerHTML = "";
+    
+    // reseto laukumu
+    while (document.getElementById("cardsID").hasChildNodes()){
+        document.getElementById("cardsID").removeChild(document.getElementById("cardsID").lastChild);
+        console.log("remove");
+    }
+    cards = [];
+//    openedCards = [];
+    
+    // veic sajaukšanu
     initialShuffle();
     inputValue();
     shuffling(cards);
+    
+    // reseto laiku
+    sec = 0;
+    min = 0; 
+    hour = 0;
+    var timer = document.querySelector(".timer");
+    timer.innerHTML = "0 min 0 sek";
+    clearInterval(interval);
+    
+    //uzsāk laika atskaiti
     startTimer();
+    
+    //spēle
     cardCheck();
 }
 
